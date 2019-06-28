55cbf1fd29daea8d03ed4f06449263f2880c9614
anolinrichard <richardii.anolin.iics@ust.edu.ph>
2019-06-28 19:44:02 +0800

    Initial Commit

--- app/src/main/java/com/example/richardrodulfo/quiz2/MainActivity.java
+++ app/src/main/java/com/example/richardrodulfo/quiz2/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cancelButton(View v){
+        Toast toast = Toast.makeText(this,"Cancel Button was clicked...",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void proceedButton(View buton){
+        Snackbar sbar = Snackbar.make(buton, "Proceed Button was clicked...", Snackbar.LENGTH_LONG);
+        sbar.show();
     }
 }
